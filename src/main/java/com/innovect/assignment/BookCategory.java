e463eeed01a5b549d6f43c70f69106d10c2f853a
Your Name <amoldhope1@gmail.com>
2019-09-29 12:39:32 +0530

    update

---
+++ src/main/java/com/innovect/assignment/BookCategory.java
@@ -0,0 +1,11 @@
+package main.java.com.innovect.assignment;
+
+public enum BookCategory {
+    TECHNICAL("TECHNICAL"),hORROR("HORROR");
+
+    private final String category;
+
+    BookCategory(String category) {
+    this.category=category;
+    }
+}
